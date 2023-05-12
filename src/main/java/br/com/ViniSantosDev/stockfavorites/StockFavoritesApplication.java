62734778cb658ea076b0f204e2f7a3009378fefb
Vinicius Santos <viniciusilvasantos.contact@gmail.com>
2023-05-12 16:54:34 -0300

    app

---
+++ src/main/java/br/com/ViniSantosDev/stockfavorites/StockFavoritesApplication.java
@@ -0,0 +1,13 @@
+package br.com.ViniSantosDev.stockfavorites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockFavoritesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockFavoritesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ViniSantosDev/stockfavorites/StockFavoritesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ViniSantosDev.stockfavorites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockFavoritesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
